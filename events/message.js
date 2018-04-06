9eea5ece302fdeb369a63554bac336581a7a968e
Seth Stephens <relative@airmail.cc>
2018-04-06 11:21:21 -0400

    Change user, guild to msg.author,msg.guild

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
 module.exports = async (client, handler, msg) => {
   // TODO: DB initializing for user & guild
-  await handler.onMessage(msg, user, guild)
+  await handler.onMessage(msg, msg.author, msg.guild)
 }
